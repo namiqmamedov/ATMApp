2283c0062e61de65a88b9b53f599244e0b1eb27b
namiqmamedov <namigmm@code.edu.az>
2023-05-14 16:42:03 +0400

    Created Domain,UI Folder & Updated Program.cs

---
+++ ConsoleApp15/App/Program.cs
@@ -0,0 +1,27 @@
+﻿using ATMApp.Domain.Entities;
+using ATMApp.UI;
+using System;
+using System.Collections.Generic;
+
+namespace ATMApp.App
+{
+    class Program
+    {
+        private List<UserAccount> userAccountList;
+        private UserAccount selectedAccount;
+
+        public void InitializeData()
+        {
+            userAccountList = new List<UserAccount>();
+        }
+
+        static void Main(string[] args)
+        {
+           AppScreen.Welcome();
+           long cardNumber  = Validator.Convert<long>("your card number");
+           Console.WriteLine($"Your card number is {cardNumber}");
+
+           Utility.PressEnterToContinue();
+        }
+    }
+}
---
+++ ConsoleApp15/UI/AppScreen.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMApp.UI
+{
+    public static class AppScreen
+    {
+        internal static void Welcome()
+        {
+            Console.Clear();
+            Console.Title = "My ATM App";
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine("\n\n--------------Welcome to My ATM App--------------\n\n");
+
+            Console.WriteLine("Please insert your ATM card");
+            Console.WriteLine("Note: Actual ATM machine will accept and validate a physical ATM card," +
+                "read the card number and validate it.");
+
+            Utility.PressEnterToContinue();
+        }
+    }
+    
+}
